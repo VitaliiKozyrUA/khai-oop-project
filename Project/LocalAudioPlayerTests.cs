622c9664fc5ba7365a3895a8ff15d9c7ff45be42
Vitalii Kozyr <vitalii.kozyr.ua@gmail.com>
2023-11-16 14:42:53 +0200

    Feature: add tests

---
+++ Project/LocalAudioPlayerTests.cs
@@ -0,0 +1,70 @@
+﻿using NAudio.Wave;
+
+namespace Project;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+[TestClass]
+public class LocalAudioPlayerTests
+{
+    [TestMethod]
+    public void Play_UpdatesPlaybackState()
+    {
+        var audioPlayer = new LocalAudioPlayer();
+        var audioFile = new FileInfo("test_audio.mp3");
+        var audio = new Audio(audioFile, AudioFormat.Mp3, "Name", "Author");
+
+        audioPlayer.Load(audio);
+
+        audioPlayer.Play();
+
+        Assert.AreEqual(PlaybackState.Playing, audioPlayer.AudioStateObservable.Value!.PlaybackState);
+    }
+
+    [TestMethod]
+    public void Pause_UpdatesPlaybackState()
+    {
+        var audioPlayer = new LocalAudioPlayer();
+        var audioFile = new FileInfo("test_audio.mp3");
+        var audio = new Audio(audioFile, AudioFormat.Mp3, "Name", "Author");
+
+        audioPlayer.Load(audio);
+        audioPlayer.Play();
+        
+        audioPlayer.Pause();
+        
+        Assert.AreEqual(PlaybackState.Paused, audioPlayer.AudioStateObservable.Value!.PlaybackState);
+    }
+
+    [TestMethod]
+    public void Seek_ChangesCurrentPosition()
+    {
+        var audioPlayer = new LocalAudioPlayer();
+        var audioFile = new FileInfo("test_audio.mp3");
+        var audio = new Audio(audioFile, AudioFormat.Mp3, "Name", "Author");
+
+        audioPlayer.Load(audio);
+        var newPosition = TimeSpan.FromSeconds(10);
+
+        audioPlayer.Seek(newPosition);
+
+        Assert.AreEqual(newPosition, audioPlayer.AudioStateObservable.Value!.CurrentPosition);
+    }
+
+    [TestMethod]
+    public void SetVolume_ChangesVolume()
+    {
+        var audioPlayer = new LocalAudioPlayer();
+        var audioFile = new FileInfo("test_audio.mp3");
+        var audio = new Audio(audioFile, AudioFormat.Mp3, "Name", "Author");
+
+        audioPlayer.Load(audio);
+        var newVolume = 0.5f;
+        
+        audioPlayer.SetVolume(newVolume);
+        
+        Assert.AreEqual(newVolume, audioPlayer.AudioStateObservable.Value!.Volume);
+    }
+}
---
+++ Project/SettingsManagerTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Project;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class SettingsManagerTests
+{
+    [TestMethod]
+    public void SettingsObservable_NotNull()
+    {
+        var settingsObservable = SettingsManager.SettingsObservable;
+        
+        Assert.IsNotNull(settingsObservable);
+    }
+
+    [TestMethod]
+    public void SettingsObservable_DefaultSettings()
+    {
+        var settingsObservable = SettingsManager.SettingsObservable.Value;
+
+        Assert.AreEqual("", settingsObservable.AudioDirectory);
+        Assert.IsTrue(settingsObservable.UseLocalAudioDirectory);
+    }
+
+    [TestMethod]
+    public void SaveSettings_UpdatesSettings()
+    {
+        var newSettings = new Settings("directory", false);
+        
+        SettingsManager.SaveSettings(newSettings);
+        var updatedSettings = SettingsManager.SettingsObservable.Value;
+
+        Assert.AreEqual(newSettings.AudioDirectory, updatedSettings.AudioDirectory);
+        Assert.AreEqual(newSettings.UseLocalAudioDirectory, updatedSettings.UseLocalAudioDirectory);
+    }
+}
